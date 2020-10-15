d7d6666b2816e41a643fdb1f3d806496febb7be8
Seun Olukayode <oluwasheeun@gmail.com>
2020-10-15 23:10:01 +0100

    Completed, just the streaming functionality left

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -35,7 +35,9 @@ const StreamList = props => {
           {renderAdmin(stream)}
           <i className='large middle aligned icon camera'></i>
           <div className='content'>
-            {stream.title}
+            <Link to={`/streams/${stream.id}`} className='header'>
+              {stream.title}
+            </Link>
             <div className='description'>{stream.description}</div>
           </div>
         </div>
--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
+const StreamShow = props => {
+  useEffect(() => {
+    props.fetchStream(props.match.params.id);
+
+    // eslint-disable-next-line
+  }, []);
+
+  if (!props.stream) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      Stream Show
+      <h1>{props.stream.title}</h1>
+      <h5>{props.stream.description}</h5>
     </div>
-  )
-}
+  );
+};
 
-export default StreamShow
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.stream[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
+
+// const NodeMediaServer = require('node-media-server');
