a1dc746249f717202886293ee72ba27e5e1fe11a
Seun Olukayode <oluwasheeun@gmail.com>
2020-10-16 01:35:39 +0100

    Completed

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,13 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 const StreamShow = props => {
+  const { id } = props.match.params;
+  const videoRef = useRef();
+
   useEffect(() => {
-    props.fetchStream(props.match.params.id);
+    props.fetchStream(id);
+    buildPlayer();
 
     // eslint-disable-next-line
   }, []);
+
+  const buildPlayer = () => {
+    if (!props.stream) {
+      return;
+    }
+
+    const player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+
+    player.attachMediaElement(videoRef.current);
+    player.load();
+  };
 
   if (!props.stream) {
     return <div>Loading...</div>;
@@ -15,6 +34,7 @@ const StreamShow = props => {
 
   return (
     <div>
+      <video ref={videoRef} style={{ width: '100%' }} controls />
       <h1>{props.stream.title}</h1>
       <h5>{props.stream.description}</h5>
     </div>
@@ -26,5 +46,3 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 export default connect(mapStateToProps, { fetchStream })(StreamShow);
-
-// const NodeMediaServer = require('node-media-server');
